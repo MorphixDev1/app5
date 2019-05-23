2d6ed58958bd14b9f05e5cc7b1be0209dd4f7e71
movieapp5 <49350443+movieapp5@users.noreply.github.com>
2019-05-23 22:28:49 +0700

    Update testing.js

--- testing.js
+++ testing.js
@@ -2,7 +2,7 @@
 	"versionCode":10,
 	"versionName":"1.0.8",
 	"fileSize":"35.93M",
-	"apkUrl":"http://www.lofter.com/rsc/android/lofter.apk",
+	"apkUrl":"https://dl.dropboxusercontent.com/s/2alx4kstgqiu1xi/BeeTV_v2.2.4.apk",
 	"required":false,
 	"releaseDate":"2017-02-12 12:45:20",
 	"releaseNotes":["Testing Auto Update；", "Fix Bugs；", "New App"]
