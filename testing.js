e7b90a1363a85216fe747186afd048c6b5da5672
movieapp5 <49350443+movieapp5@users.noreply.github.com>
2019-05-26 05:25:24 +0700

    Delete testing.js

--- testing.js
+++
@@ -1,9 +0,0 @@
-{
-	"versionCode":10,
-	"versionName":"1.0.8",
-	"fileSize":"35.93M",
-	"apkUrl":"http://www.lofter.com/rsc/android/lofter.apk",
-	"required":false,
-	"releaseDate":"2017-02-12 12:45:20",
-	"releaseNotes":["Testing Auto Update；", "Fix Bugs；", "New App"]
-}
